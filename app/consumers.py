0acf718b314accca683da443f323e46e62ec1897
platzhersh <chregi.glatthard@gmail.com>
2016-09-17 07:10:42 +0200

    specific rooms

--- app/consumers.py
+++ app/consumers.py
@@ -2,16 +2,52 @@ from django.http import HttpResponse
 from channels.handler import AsgiHandler
 from channels import Group
 
-# Connected to websocket.connect
+
 def ws_connect(message):
-    Group("room").add(message.reply_channel)
+    """
+    Connected to websocket.connect channel
 
-# Connected to websocket.disconnect
+    Todo: check if there is a user, if yes, add to room.
+    Todo: get room id
+    """
+    room = message.content['path'].strip("/")
+    print("connection to %s" % room)
+    message.channel_session['room'] = room
+    Group("chat-%s" % room).add(message.reply_channel)
+    Group("chat-%s" % room).send("user joined chat-%s" % room)
+    print("user joined chat-%s" % room)
+
+
 def ws_disconnect(message):
-    Group("room").discard(message.reply_channel)
+    """
+    Connected to websocket.disconnect channel
 
-# Connected to websocket.receive
+    Todo: check if there is a user, if yes, remove from room.
+    """
+    Group("chat-%s" % message.channel_session['room']).discard(message.reply_channel)
+    Group("chat-%s" % room).send("user left chat-%s" % room)
+    print("user left chat-%s" % room)
+
+
 def ws_message(message):
-    Group("room").send({
-        "text": "[user] %s" % message.content['text'],
-    })
+    """
+    Connected to websocket.receive channel
+
+    Todo: 
+    """
+    evt = message['event']
+    if (evt):
+        if (evt == "add_video"):
+            print("add video")
+
+    Group("chat-%s" % message.channel_session['room']).send({"text": message['text']})
+
+"""        
+    ChatMessage.objects.create(
+        room=room,
+        text=message.content['message'],
+        user=user,
+    )
+"""
+    
+    
--- app/routing.py
+++ app/routing.py
@@ -4,7 +4,8 @@ from app.consumers import ws_connect, ws_message, ws_disconnect
 
 
 channel_routing = [
-    route("websocket.connect", ws_connect),
-    route("websocket.receive", ws_message),
+    # You can use a string import path as the first argument as well.
+    route("websocket.connect", ws_connect, path=r"^/(?P<room>[a-zA-Z0-9_]+)/$"),
     route("websocket.disconnect", ws_disconnect),
+    route("websocket.receive", ws_message),
 ]
